ac3345078b365bf2c733235f1b7475c5958e7b9e
Christopher Fonseka <chris.fonseka@googlemail.com>
2024-12-11 00:39:12 +0000

    Day 3, Part 2

--- AoC 3/AoC 3/Logic.swift
+++ AoC 3/AoC 3/Logic.swift
@@ -7,6 +7,10 @@
 
 import RegexBuilder
 
+let doRegex = Regex { "do()" }
+
+let dontRegex = Regex { "don't()" }
+
 let mulFuctionRegex = Regex {
     "mul("
     Capture{
@@ -21,6 +25,81 @@ let mulFuctionRegex = Regex {
     } transform: { match in
         Int(match)
     }
-
     ")"
+}
+
+final class RegexMatchSequence: Sequence, IteratorProtocol {
+
+    enum Match {
+        case `do`
+        case dont
+        case mult(Int, Int)
+    }
+
+    struct MatchAndRange {
+        let match: Match
+        let range: Range<String.Index>
+    }
+
+    let input: String
+    var ptr: String.Index
+
+    init(input: String) {
+        self.input = input
+        self.ptr = input.startIndex
+    }
+
+    private var _input: String.SubSequence {
+        input[ptr...]
+    }
+
+    func next() -> Match? {
+        var mulMatch: MatchAndRange? {
+            guard
+                let reg = _input.firstMatch(of: mulFuctionRegex),
+                let lhs = reg.output.1, let rhs = reg.output.2
+            else { return nil }
+            let match = Match.mult(lhs, rhs)
+            return MatchAndRange(match: match, range: reg.range)
+        }
+
+        var doMatch: MatchAndRange? {
+            guard let reg = _input.firstMatch(of: doRegex) else { return nil }
+            return MatchAndRange(match: .do, range: reg.range)
+        }
+
+        var dontMatch: MatchAndRange? {
+            guard let reg = _input.firstMatch(of: dontRegex) else { return nil }
+            return MatchAndRange(match: .dont, range: reg.range)
+        }
+
+        let nextMatch = [mulMatch, doMatch, dontMatch]
+            .compactMap { $0 }
+            .min(by: { $0.range.lowerBound < $1.range.lowerBound })
+
+        if let nextMatch {
+            self.ptr = nextMatch.range.upperBound
+            return nextMatch.match
+        } else {
+            return nil
+        }
+    }
+}
+
+extension RegexMatchSequence.Match: CustomStringConvertible {
+
+    var description: String {
+        switch self {
+            case .do: "do"
+            case .dont: "dont"
+            case .mult(let lhs, let rhs): "mul(\(lhs), \(rhs))"
+        }
+    }
+}
+
+extension RegexMatchSequence.MatchAndRange: CustomStringConvertible {
+
+    var description: String {
+        "(m:\(match), r:\(range.description))"
+    }
 }
--- AoC 3/AoC 3/main.swift
+++ AoC 3/AoC 3/main.swift
@@ -8,16 +8,39 @@
 import Foundation
 import RegexBuilder
 
-let result = challenge
-    .matches(of: mulFuctionRegex)
-    .compactMap { result -> Int? in
-        guard
-            let lhs = result.output.1,
-            let rhs = result.output.2
-        else { return nil }
+// Part 1
+//let result = input
+//    .matches(of: mulFuctionRegex)
+//    .compactMap { result -> Int? in
+//        guard
+//            let lhs = result.output.1,
+//            let rhs = result.output.2
+//        else { return nil }
+//
+//        print(lhs, rhs)
+//        return lhs * rhs
+//    }
+//    .reduce(0, +)
+//
 
-        return lhs * rhs
+// Part 2
+
+let seq = RegexMatchSequence(input: challenge)
+
+var acc = 0
+var disabledInstructions = false
+
+while let result = seq.next() {
+    switch result {
+    case .do:
+        disabledInstructions = false
+    case .dont:
+        disabledInstructions = true
+    case .mult(let int, let int2):
+        guard !disabledInstructions else { continue }
+        let result = int * int2
+        acc += result
     }
-    .reduce(0, +)
+}
 
-print(result)
+print("result is \(acc)")
